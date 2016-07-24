aa8169a8848ad074ea3371537d01dc4e119ecb1e
qianlifeng <qianlf2008@163.com>
2016-07-25 02:05:29 +0800

    Allow user specify config path

--- src/main.go
+++ src/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/jasonlvhit/gocron"
 	"github.com/parnurzeal/gorequest"
 	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
 )
 
@@ -19,13 +18,13 @@ type Config struct {
 }
 
 func uploadStatus(config Config) {
+	fmt.Println("Upload status...")
 	v, _ := mem.VirtualMemory()
 	c, _ := cpu.Percent(0, false)
-	d, _ := disk.Usage("c:")
+	//d, _ := disk.Usage("c:")
 
-	fmt.Println("c:", c)
-	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f\n", v.Total, v.Free, v.UsedPercent)
-	fmt.Printf("HD: %v GB  Free: %v GB Usage:%f\n", d.Total/1024/1024/1024, d.Free/1024/1024/1024, d.UsedPercent)
+	// fmt.Printf("Total: %v, Free:%v, UsedPercent:%f\n", v.Total, v.Free, v.UsedPercent)
+	// fmt.Printf("HD: %v GB  Free: %v GB Usage:%f\n", d.Total/1024/1024/1024, d.Free/1024/1024/1024, d.UsedPercent)
 
 	jsonBody := fmt.Sprintf(`{"nodeCode":"%s", "nodeStatus":{"status":"up","metrics":[`+
 		`{"title":"Mem","type":"gauge","value": %f,"width":0.25}`+
@@ -36,14 +35,33 @@ func uploadStatus(config Config) {
 }
 
 func main() {
-	file, _ := os.Open("config.json")
+	configPath := "config.json"
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	}
+	if _, err := os.Stat(configPath); os.IsExist(err) {
+		fmt.Println("config file: " + configPath + " not exist!")
+		return
+	}
+
+	file, _ := os.Open(configPath)
 	decoder := json.NewDecoder(file)
 	configuration := Config{}
 	err := decoder.Decode(&configuration)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("Read config file error: ", err)
+		return
 	}
-	fmt.Printf("config: %#v", configuration)
+
+	if configuration.Server == "" {
+		fmt.Println("Server can't be empty")
+		return
+	}
+	if configuration.Node == "" {
+		fmt.Println("Node can't be empty")
+		return
+	}
+
 	gocron.Every(1).Seconds().Do(uploadStatus, configuration)
 	<-gocron.Start()
 }
